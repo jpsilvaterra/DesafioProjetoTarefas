18102adb33688189ebc6a408851529e74953732a
João Pedro Silva Terra <101167595+jpsilvaterra@users.noreply.github.com>
2023-01-14 21:55:59 -0300

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace curso_dio_desafio_projeto_tarefasmvc_Pottencial.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
